0c6f1a9e583c07d7c6970bd8685676b8dcad05f1
9QIX <kirkhendrixbulaong@outlook.com>
2024-02-09 05:56:29 +0800

    Update hero title in Hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -26,8 +26,14 @@ export default function Hero() {
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-              Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">DERMAFY</span>
+            <h4 className="text-5xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+              Welcome to
+            </h4>
+            <h1
+              className="text-12xl md:text-6xl font-extrabold bg-clip-text tracking-tighter mb-4 text-transparent bg-gradient-to-r from-blue-500 to-teal-400"
+              data-aos="zoom-y-out"
+            >
+              DERMAFY
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
